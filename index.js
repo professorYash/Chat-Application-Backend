4c7b2e451bf380104928c339aa4aea62ec0d0a31
Yash86400-Design <knowledge86400@gmail.com>
2024-01-08 23:44:05 +0530

    Separated the mongodb connection in different file

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
+const connectDB = require("./models/db");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connection Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+// Connection to database
+connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
