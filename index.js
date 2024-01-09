41f4df3cdf3dadb27621870fed06e7fe6dd62fcf
Yash86400-Design <knowledge86400@gmail.com>
2024-01-09 10:33:38 +0530

    Added allowed origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.ALLOWED_ORIGIN,
     credentials: true,
   },
 });
