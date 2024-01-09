744e039bcd78481bcba3422ba9086d8b58d8accc
Yash86400-Design <knowledge86400@gmail.com>
2024-01-09 10:35:58 +0530

    added origin to cors also

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const socket = require("socket.io");
 const connectDB = require("./models/db");
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGIN
+}));
 app.use(express.json());
 
 // Connection to database
